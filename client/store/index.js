8a1e27a7ef6566217dbb1c144a5d8ba6a009bf8b
kaiogita <kaiogita@gmail.com>
2018-06-08 02:23:11 +0900

    APIをquery strgin対応

--- client/store/index.js
+++ client/store/index.js
@@ -2,18 +2,21 @@ import Vuex from 'vuex'
 
 const store = () => new Vuex.Store({
     state: {
-        oneDayRanking:[]
+        oneDayRanking:[],
+        rankingMeta:[]
     },
     mutations: {
     },
     getters:{
     },
     actions: {
-        GET_ONE_DAY_RANKING({state, commit}, {id}){
-            return this.$axios.$get(`api/vt/one-day-ranking`)
+        GET_ONE_DAY_VIEW_RANKING({state, commit}, {page}){
+            console.log(`api/vt/one-day-ranking?page=${page}`);
+            return this.$axios.$get(`api/vt/one-day-ranking?page=${page}`)
                 .then((response) => {
-                    console.log(response);
+                    //console.log(response);
                     state.oneDayRanking = response.data
+                    state.rankingMeta = response.meta
                 }).catch((error) => {
                     throw new Error("a");
                 });

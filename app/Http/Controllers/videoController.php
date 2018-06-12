b26cd1b3108da4e77d779ba82946d0520d70a71f
kaiogita <kaiogita@gmail.com>
2018-06-13 01:42:39 +0900

    channelIdからvideoを取得するAPI追加

--- app/Http/Controllers/videoController.php
+++ app/Http/Controllers/videoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\VideoResource;
 use App\Models\Video;
+use function foo\func;
 use Illuminate\Http\Request;
 
 class videoController extends Controller
@@ -18,6 +19,14 @@ class videoController extends Controller
     public function recent()
     {
         $data = $this->video->orderBy("published_at", "desc")->paginate(5);
+        return VideoResource::Collection($data);
+    }
+
+    public function channelId($id)
+    {
+        $data = $this->video->whereHas("youtuber" ,function($q) use ($id){
+                $q->where("channel_id", $id);
+        })->orderBy("published_at", "desc")->paginate(5);
         return VideoResource::Collection($data);
     }
 }

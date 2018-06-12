071aba1753ecec1d2a11cde1b5f4efac2f3afbfc
kaiogita <kaiogita@gmail.com>
2018-06-13 02:08:40 +0900

    video一覧とmodal表示追加

--- client/pages/channel/_id.vue
+++ client/pages/channel/_id.vue
@@ -8,9 +8,9 @@
                     </h1>
                 </v-flex>
                 <v-flex xs8 md8 lg8>
-                    <v-card>
+                    <v-card v-if="channel_video.length > 0">
                         <div class="yy" style="">
-                            <iframe width="100%" height="100%" src="//www.youtube.com/embed/CcdBKdhLBpk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <iframe width="100%" height="100%" :src="getAutoPlayiframe(channel_video[0].video_id)" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                         </div>
                     </v-card>
                 </v-flex>
@@ -21,24 +21,82 @@
                             <div>
 
                             </div>
-                            <p>
+
                                 <v-chip label color="pink" text-color="white">
                                     チャンネル登録者数 {{channel.view_count}} 人
                                 </v-chip>
-                            </p>
-                            <p>
+
+
                                 <v-chip label color="pink" text-color="white">
                                     総視聴回数 {{channel.subscriber_count}} 回
                                 </v-chip>
-                            </p>
+
+
+                                <v-chip label color="pink" text-color="white">
+                                    総投稿数 {{channel.video_count}} 本
+                                </v-chip>
+
                         </v-card-title>
-                        <v-card-actions>
-                            <v-btn flat dark>Listen now</v-btn>
-                        </v-card-actions>
                     </v-card>
                 </v-flex>
             </v-layout>
         </v-container>
+        <v-container grid-list-md>
+            <v-layout row wrap>
+                <v-card color="white">
+                    <!--<v-layout row wrap align-baseline>-->
+
+                    <!--<v-flex md3 lg3  v-for="iaa in 30" :key="iaa">-->
+                    <!--<v-card color="blue-grey darken-2" class="white&#45;&#45;text">-->
+                    <!--<v-card-media src="https://i.ytimg.com/vi/MuFxkZMtv_M/hqdefault.jpg"></v-card-media>-->
+                    <!--</v-card>-->
+                    <!--</v-flex>-->
+
+                    <!--</v-layout>-->
+                    <v-container grid-list-md text-xs-center>
+                        <v-layout row wrap>
+                            <v-flex fill-height xs4 md4 lg4 v-for="(video, index) in channel_video" :key="index" @click="openModal(index)">
+                                <v-card raised >
+                                    <v-card-media
+                                            class="white--text"
+                                            height="160px"
+                                            :src="video.thumbnail_high"
+                                    >
+                                    </v-card-media>
+                                    <v-card-title>
+                                        <div>
+                                            <span>{{video.title}}</span><br>
+                                            <p class="grey--text text-xs-right mb-0">{{video.youtuber.nickname}}</p>
+                                        </div>
+                                    </v-card-title>
+                                </v-card>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                    <div class="text-xs-center">
+                        <v-pagination :length="this.channelVideoMeta.last_page" v-model="channelVideoMeta.current_page"
+                                      :total-visible="7" @input="handlePaginate"></v-pagination>
+                    </div>
+                </v-card>
+            </v-layout>
+        </v-container>
+        <v-dialog v-model="dialog" max-width="500px">
+            <v-card color="white">
+                <v-card-title>
+                    {{this.modalContent.title}}
+                    <div class="yy" style="">
+                        <iframe width="100%" height="100%" :src="getAutoPlayiframe(this.modalContent.video_id)" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </div>
+                </v-card-title>
+                <v-card-text>
+                    <v-btn color="primary" dark >Open Dialog 3</v-btn>
+                    aaaaa
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" flat @click.stop="dialog=false">Close</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-content>
 </template>
 
@@ -87,20 +145,35 @@
         components: {Tabs},
         data() {
             return {
-                drawer: null,
-
-                page:1,
+                dialog: false,
+                modalContent:{
+                    title:"",
+                    video_id:""
+                }
             }
         },
         computed: {
             ...mapState([
                 "channel",
-                "channel_video"
+                "channel_video",
+                "channelVideoMeta"
             ])
         },
         methods:{
+            openModal(index){
+                this.modalContent = this.channel_video[index];
+                this.dialog = true;
+            },
+            handlePaginate(page){
+                console.log($nuxt.$route.path);
+                console.log(page);
+                window.location.href = `${$nuxt.$route.path}?page=${page}`;
+            },
+            getAutoPlayiframe(id){
+                return `//www.youtube.com/embed/${id}?autoplay=1`
+            }
         },
-        async asyncData({store, params, error}) {
+        async asyncData({query, store, params, error}) {
 
             console.log("aaaaaaaaaa");
             await store.dispatch("GET_CHANNEL", {id: params.id}).catch(() => {
@@ -108,7 +181,7 @@
             });
 
             console.log("wwwwwwwwww");
-            await store.dispatch("GET_VIDEO_BY_ID").catch(() => {
+            await store.dispatch("GET_VIDEO_BY_ID",{page: query.page}).catch(() => {
                 error({statusCode: 404, message: 'Post2 not found'});
             });
 

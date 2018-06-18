09804ac60df3e6e75eeafd2714012b13fe4d1ec7
kaiogita <kaiogita@gmail.com>
2018-06-19 00:57:20 +0900

    spのデザイン変更

--- client/components/rankingLists.vue
+++ client/components/rankingLists.vue
@@ -1,15 +1,15 @@
 <template>
-    <v-flex xs10 offset-xs1>
-        <v-container grid-list-md text-xs-center>
-            <v-layout row wrap align-baseline v-for="(item, index) in lists" :key="index">
-                <v-flex xs1>
-                    <v-list-tile-avatar color="rankBox" class="justify-center">
+    <!--<v-flex>-->
+        <v-container>
+            <v-layout row wrap align-baseline v-for="(item, index) in lists" :key="index" justify-center>
+                <v-flex xs2 sm1 md1 lg1>
+                    <v-list-tile-avatar color="rankBox" class="">
                         <div>
                             <span>{{index + 1}}</span>
                         </div>
                     </v-list-tile-avatar>
                 </v-flex>
-                <v-flex xs1>
+                <v-flex xs3 sm1 md2 lg1>
                     <v-avatar
                             slot="activator"
                             size="50px"
@@ -18,19 +18,20 @@
                     </v-avatar>
                 </v-flex>
 
-                <v-flex xs3>
+                <v-flex xs4 sm3 md4 lg3>
                     <div class="subheading">{{item.nickname}}</div>
                     <div class="title grey--text text--darken-1 caption">{{item.channel_title}}
                     </div>
                 </v-flex>
-                <v-flex xs3>
-                    <v-card-title primary-title>
-                        <v-list-tile-title>{{item.view_count}}view</v-list-tile-title>
-                        <v-list-tile-sub-title class="teal--text caption">+ {{item.increase_count}}
-                        </v-list-tile-sub-title>
-                    </v-card-title>
+                <v-flex xs3 sm3 md4 lg3>
+                    <!--<v-card-title primary-title>-->
+                        <!--<v-list-tile-title>{{item.view_count}}view</v-list-tile-title>-->
+                        <!--<v-list-tile-sub-title class="teal&#45;&#45;text caption">+ {{item.increase_count}}</v-list-tile-sub-title>-->
+                    <!--</v-card-title>-->
+                    <v-list-tile-title>{{item.view_count}}view</v-list-tile-title>
+                    <v-list-tile-sub-title class="teal--text caption">+ {{item.increase_count}}</v-list-tile-sub-title>
                 </v-flex>
-                <v-flex xs4>
+                <v-flex hidden-md-and-down lg4>
                     <v-list three-line>
                         <v-list-tile-content>
                             <v-list-tile-sub-title class="grey--text caption"
@@ -40,10 +41,10 @@
                 </v-flex>
             </v-layout>
         </v-container>
-        <div class="text-xs-center">
-            <v-pagination :length="this.rankingMeta.last_page" v-model="rankingMeta.current_page" :total-visible="7" @input="handlePaginate"></v-pagination>
-        </div>
-    </v-flex>
+        <!--<div class="text-xs-center">-->
+            <!--<v-pagination :length="this.rankingMeta.last_page" v-model="rankingMeta.current_page" :total-visible="7" @input="handlePaginate"></v-pagination>-->
+        <!--</div>-->
+    <!--</v-flex>-->
 </template>
 
 <script>
